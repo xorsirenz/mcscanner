f2cf0664917bd9140d641d2b4c58b8506e3c4bb2
madison <69394836+xorsirenz@users.noreply.github.com>
2026-01-13 00:01:54 -0800

    rm unneeded dumps/loads

--- src/bluemap_logger.py
+++ src/bluemap_logger.py
@@ -7,18 +7,17 @@ def get_response(url):
     chat_messages = []
 
     r = requests.get(url)
-    json_res = r.json()
-    json_dump = json.dumps(json_res)
-    data = json.loads(json_dump)
+    data = r.json()
 
     for _, value in data['chat-markers']['markers'].items():
         chat_messages.append((value['label']))
     return chat_messages
 
 def monitor_chat():
-    url = input(f"[*] markers.json url:\n[>] ")
+    url = input("[*] markers.json url:\n[>] ")
     print("[*] waiting for new messages\n")
     last_messages = get_response(url)
+
     while True:
         current_messages = get_response(url)
         if current_messages != last_messages:
